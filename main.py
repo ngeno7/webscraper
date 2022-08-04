f8b2f79d611e8639bf2c0eeaa7c3dc7ef58f3939
hngeno <hngeno@hngeno-HP-ProBook-440-G1>
2022-08-04 22:12:02 +0300

    add export to csv

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import csv
 
 from bs4 import BeautifulSoup
 
@@ -9,16 +10,23 @@ soup = BeautifulSoup(page.content, "html.parser")
 
 results = soup.find(id="ResultsContainer")
 
-print(results.prettify())
-
 job_elements = results.find_all("div", class_="card-content")
 
+rows = []
+with open('fake-jobs.csv', 'a') as csv_file:
+    writer = csv.writer(csv_file)
+    writer.writerow([
+            'Title',
+            'Company',
+            'Location'
+        ])
+    for job_element in job_elements:
+        title_element = job_element.find("h2", class_="title")
+        company_element = job_element.find("h3", class_="company")
+        location_element = job_element.find("p", class_="location")
 
-for job_element in job_elements:
-    title_element = job_element.find("h2", class_="title")
-    company_element = job_element.find("h3", class_="company")
-    location_element = job_element.find("p", class_="location")
-    print(title_element)
-    print(company_element)
-    print(location_element)
-    print()
+        writer.writerow([
+            title_element.text.strip(),
+            company_element.text.strip(),
+            location_element.text.strip()
+        ])
